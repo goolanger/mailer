c45002351559634a0ced8df2846012d34274c515
Amaury Diaz <amauryuh@gmail.com>
2019-02-18 19:45:31 -0500

    and buidling...

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+)
+
+func main() {
+	parser := argparse.NewParser("smailey", "SMailey provides a simple and robust email solution to newsletter subscriptions and notices. Implements a variety of algorithms that consist in concurrency tasks managements and fake mail detections.")
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+	}
+}
